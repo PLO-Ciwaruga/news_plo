a8cd85a6635d7c42d4393d6bb835d685682f37c1
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2020-07-17 01:31:16 +0700

    Fix Tampilan Data pada Tabel

--- assets/js/news.js
+++ assets/js/news.js
@@ -2,56 +2,60 @@ $(document).ready(function(){
     $.getJSON("annotation/result.json", function(data){
         var news = '';
         $.each(data, function(key, value){
-            news += '<tr>';
             if(value.completions[0].result[0].value.choices == "Clickbait"){
+                news += '<tr>';
                 news += '<td>'+value.data.sumber+'</td>';
                 news += '<td>'+value.data.judul+'</td>';
+                news += '</tr>';
             }
-           
-            news += '</tr>';
         });
         $('#clickbait_table').append(news);
+    }).done(function() {
+        $('.clickbait_tbl').footable(); 
     });
 
     $.getJSON("annotation/result.json", function(data){
         var news = '';
         $.each(data, function(key, value){
-            news += '<tr>';
             if(value.completions[0].result[1].value.choices == "Misleading"){
+                news += '<tr>';
                 news += '<td>'+value.data.sumber+'</td>';
                 news += '<td>'+value.data.judul+'</td>';
+                news += '</tr>';
             }
-           
-            news += '</tr>';
         });
         $('#misleading_table').append(news);
+    }).done(function() {
+        $('.misleading_tbl').footable(); 
     });
 
     $.getJSON("annotation/result.json", function(data){
         var news = '';
         $.each(data, function(key, value){
-            news += '<tr>';
             if(value.completions[0].result[1].value.choices == "Not Misleading"){
+                news += '<tr>';
                 news += '<td>'+value.data.sumber+'</td>';
                 news += '<td>'+value.data.judul+'</td>';
+                news += '</tr>';
             }
-           
-            news += '</tr>';
         });
         $('#notMisleading_table').append(news);
+    }).done(function() {
+        $('.notMisleading_tbl').footable(); 
     });
 
     $.getJSON("annotation/result.json", function(data){
         var news = '';
         $.each(data, function(key, value){
-            news += '<tr>';
             if(value.completions[0].result[0].value.choices == "Neutral"){
+                news += '<tr>';
                 news += '<td>'+value.data.sumber+'</td>';
                 news += '<td>'+value.data.judul+'</td>';
+                news += '</tr>';
             }
-           
-            news += '</tr>';
         });
         $('#nonClickbait_table').append(news);
+    }).done(function() {
+        $('.notClickbait_tbl').footable(); 
     });
 });
